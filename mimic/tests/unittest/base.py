2d6e7fd0a454cfafc1b4e5085e904477250d1930
wentian <wentian@unitedstack.com>
2013-08-20 15:43:41 +0800

    Destory the old-mimic
    Change-Id: I416e757e1b90c86253d7353c1a6fe6f709be3eb8

--- mimic/tests/unittest/base.py
+++
@@ -1,9 +0,0 @@
-import testtools
-import mox
-
-
-class TestCase(testtools.TestCase):
-
-    def setUp(self):
-        super(TestCase, self).setUp()
-        self.mox = mox.Mox()
--- tools/needed.rst
+++
@@ -1,3 +0,0 @@
-python-devel
-gcc
-memcached
