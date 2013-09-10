7375328b80c7f2891780201387d55cd403a6a7b1
wentian <wentian@unitedstack.com>
2013-09-10 11:09:51 +0800

    Add Support for switch get info
    1. get switch info
    2. check tftpboot/default
    
    Change-Id: Id4e6116af873d249da7ee0cf27feecb6f6380efd

--- mimic/api/controllers/v1/switchs.py
+++ mimic/api/controllers/v1/switchs.py
@@ -18,3 +18,24 @@ class SwitchController(rest.RestController):
                 getstatusoutput("cat /etc/mimic/local.template "
                                 "> /var/lib/tftpboot/pxelinux.cfg/default")
         return status
+
+    @wsme_pecan.wsexpose(unicode, unicode)
+    def get_all(self):
+        flag, answer_local = commands.\
+                getstatusoutput("cat /etc/mimic/local.template")
+        flag, answer_pxe = commands.\
+                getstatusoutput("cat /etc/mimic/pxe.template")
+        flag, answer_status = commands.\
+                getstatusoutput("cat /var/lib/tftpboot/pxelinux.cfg/default")
+
+        result = {}
+        if answer_local == answer_status:
+            result = {
+                "lock": False
+            }
+        else:
+            result = {
+                "lock": True
+            }
+
+        return result
