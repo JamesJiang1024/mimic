1a6be7201a412854f478c0c34ff294c5526c5c21
jiangwt100 <wentian@unitedstack.com>
2013-10-28 15:08:31 +0800

    Add update for env resource
    Change-Id: I5307d100a1ba9db329b7303269153bc730da46e6

--- mimic/api/controllers/v1/env.py
+++ mimic/api/controllers/v1/env.py
@@ -27,4 +27,32 @@ class EnvController(rest.RestController):
     def get_one(self, match):
         dbapi = api.get_instance()
         result = dbapi.find_lookup_value_by_match("env=%s" % match)
-        return result[0]["value"]
+        if len(result) > 0:
+            return result[0]["value"]
+
+    @wsme_pecan.wsexpose(unicode, unicode, body=unicode)
+    def put(self, key, content):
+        dbapi = api.get_instance()
+        result = self._update_env_key_value(key, content['value'])
+        return True
+
+    def _update_env_key_value(self, key, value):
+        """
+        update the global parameter key and value
+
+        key: global parameter key
+        value: global parameter value
+        """
+
+        dbapi = api.get_instance()
+        lookup_values = {
+            "match": "env=%s" % key,
+            "value": value,
+            "lookup_key_id": 10000
+        }
+        result = dbapi.find_lookup_value_by_match("env=%s" % key)
+        if len(result) > 0:
+            dbapi.update_lookup_value(result[0].id, lookup_values)
+        else:
+            dbapi.create_lookup_value(lookup_values)
+        return lookup_values
